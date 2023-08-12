37d96bc6ea50a2dac5ee83605b57f72a53dd454c
ibrahim <nsism99@gmail.com>
2023-08-12 12:43:51 +0300

    done

---
+++ mein.js
@@ -0,0 +1,304 @@
+// // Premetive
+
+
+// // // Boolean
+// // let text1 = false;
+// // let text2 = true;
+// // console.log(text1);
+// // console.log(text2);
+// // console.log(typeof text1);
+
+// // // Symbol
+// // let id3 = Symbol("id")
+// // let id1 = Symbol("id")
+// // console.log(id1 === id3);
+
+// // // console.log(id);
+// // // console.log(typeof id);
+
+// // // null type 
+
+// // let test3 = null;
+// // console.log(test3);
+// // console.log(typeof test3);
+
+
+// // // undefined
+// // let test4;
+// // console.log(test4);
+// // console.log(typeof test4);
+
+// // // Number
+// // // integer 
+// // let tweiq = 100;
+// // console.log(tweiq);
+// // // double
+// // let x2 = 2.567
+// // console.log(x2);
+// // console.log(typeof x2);
+
+
+// // // bigint 
+// // let big1 = 28647629846n;
+// // console.log(big1);
+// // console.log(big1);
+// // let tweiq1 = 20;
+// // console.log(tweiq1);
+
+
+
+// // let test = 'web design';
+// // let test2 = '200';
+// // console.log(test2);
+// // console.log(typeof test2);
+// // console.log(test);
+// // console.log(typeof test);
+
+// // let message0 = "ittehad'benzima' gool"
+// // let message1 = 'ittehad"goooool"roma'
+// // let message2 = "ittehad'jota' gool"
+// // let message3 = "ittehad\'cornado'\ gool"
+// // console.log(message0);
+// // console.log(message1);
+// // console.log(message2);
+// // console.log(message3);
+
+
+
+// // let path = "win" - "win"
+// // console.log(path);
+// // console.log(typeof path);
+
+// // let path2 = "34" - "7"
+// // console.log(path2);
+// // console.log(typeof path2);
+
+// // let path3 = "23" + "44"
+// // console.log(path3);
+// // console.log(typeof path3);
+
+
+// // // interpolation
+
+// // let country = "Ryiadhy"
+// // let continent = "Asia"
+// // console.log(`${country} located in ${continent}`);
+
+
+
+// // // string propreti
+
+// // // let str="java script language";
+// // // console.log(str.length);
+// // // console.log(str.charAt (0));
+// // // console.log(str.slice (0,17));
+// // // console.log(str.split(" "));
+
+
+
+// // let str = "java script language";
+
+
+// // console.log(str.length);
+// // console.log("test".length);
+
+
+ 
+// // console.log(str.charAt(0));
+// // console.log("abc".charAt(1));
+// // console.log(str.slice(0, 4));
+// // console.log("test".slice(1, 3));
+
+
+
+// // console.log(str.split(" "));
+// // console.log("138.375.27".split('.'));
+
+
+
+// // NON Premetive 
+
+
+
+// // converstion types 
+
+// let Str=String();
+// let Num=Number();
+// let Bool=Boolean();
+
+// const str="text";
+// const strToStr=String(str)
+// console.log(`${typeof str}: ${str}`);
+// console.log(`${typeof strToStr}: ${strToStr}`);
+
+// // num to str 
+// const num_1=42;
+// const numToStr=String(num_1)
+// console.log(`${typeof num_1}: ${num_1}`);
+// console.log(`${typeof numToStr}: ${numToStr}`);
+
+// // Boolean to String
+// const bool_1=true;
+// const BoolToStr=String(bool_1);
+// console.log(`${typeof bool_1}: ${bool_1}`);
+// console.log(`${typeof BoolToStr}: ${BoolToStr}`);
+
+// // undefined to String 
+
+// const Un=undefined;
+// const UnToStr=String(Un)
+// console.log(`${typeof Un}: ${Un}`);
+// console.log(`${typeof UnToStr}: ${UnToStr}`);
+
+// // Null To String 
+// const null_1=null;
+// const nullToStr=String(null_1)
+// console.log(`${typeof null_1}: ${null_1}`);
+// console.log(`${typeof nullToStr}: ${nullToStr}`);
+
+// // str to number
+// const str_1="11";
+// const NumTostr=Number(str_1)
+// // console.log(`${typeof str_1}: ${str_1}`);
+// // console.log(`${typeof NumTostr}: ${NumTostr}`);
+
+// // // str to number
+// // const str1="text";
+// // const NumToStr=Number(str1)
+// // console.log(`${typeof str1}: ${str1}`);
+// // console.log(`${typeof NumToStr}: ${NumToStr}`);
+
+// // // number to number 
+// // const num1=5;
+// // const NumToNum=Number(num1)
+// // console.log(`${typeof num1}: ${num1}`);
+// // console.log(`${typeof NumToNum}: ${NumToNum}`);
+
+
+// // // Boolean to number
+// // const bool_2=true;
+// // const BoolTonum=Number(bool_2);
+// // console.log(`${typeof bool_2}: ${bool_2}`);
+// // console.log(`${typeof BoolTonum}: ${BoolTonum}`);
+
+
+
+
+// // // undefined to bool 
+
+// // const Un_2=""
+// // const UnToBoll=Boolean(Un_2)
+// // console.log(`${typeof Un_2}: ${Un_2}`);
+// // console.log(`${typeof UnToBoll}: ${UnToBoll}`);
+
+
+// // // Null To boll
+// // const null_3=null;
+// // const nullToboll=Boolean(null_3)
+// // console.log(`${typeof null_3}: ${null_3}`);
+// // console.log(`${typeof nullToboll}: ${nullToboll}`);
+
+
+// // // str to number
+// // const str_2="text";
+// // const strToboll=Boolean(str_2)
+// // console.log(`${typeof str_2}: ${str_2}`);
+// // console.log(`${typeof strToboll}: ${strToboll}`);
+
+// // // boll to boll
+// // const boll_7="";
+// // const bollToboll=Boolean(boll_7)
+// // console.log(`${typeof boll_7}: ${boll_7}`);
+// // console.log(`${typeof bollToboll}: ${bollToboll}`);
+
+
+
+
+// // // undefined to number 
+
+// // const Un_1=undefined;
+// // const UnTonum=Boolean(Un_1)
+// // console.log(`${typeof Un_1}: ${Un_1}`);
+// // console.log(`${typeof UnTonum}: ${UnTonum}`);
+
+
+// // // Null To number 
+// // const null_2=null;
+// // const nullTonum=Boolean(null_1)
+// // console.log(`${typeof null_2}: ${null_2}`);
+// // console.log(`${typeof nullTonum}: ${nullTonum}`);
+
+
+
+// // // Automatic conversion
+
+// // const um_11=(22 + "1")
+// // console.log(um_11);
+
+
+// // const um_34=22 - "1"
+// // console.log(um_34);
+
+// // console.log("hallo , world"); 
+// // console.log("ibrahim mohammed alsharekh");
+// // console.log("15");
+// // console.log(new Date);
+// // console.log();
+
+
+// const id=1;
+
+// // let ibrahim="itte";
+// // let_ibrahim;
+
+
+// // let $ibrahim;
+
+// // let My_age;
+
+// let myname;
+// myname="ibrahim alsharekh";
+
+
+// console.log(id);
+
+
+// const My_ag7489e=14;
+
+
+// console.log(My_ag7489e)
+// console.log(itte);
+
+// object 
+// let obj={
+//     nr:600,
+//     str:"text"
+// }
+// console.log(typeof obj);
+// console.log(obj.nr);
+// console.log(obj.str);
+
+// let obj2 ={
+//  let name8="sara";
+//     let surname1="mohammed";
+//     let age1=22;
+//     let emil1"saramohamme@teleworm.us";
+
+// }
+
+// let obj3 ={
+//     let name2="salma";
+//     let surname2="ahmed";
+//     let age2=20;
+//     let emil2"salmaahmed@dayreb.com";
+
+// }
+
+
+// scop 
+// {
+// let un="jbewg";
+// console.log(un);
+
+ 
+// }
